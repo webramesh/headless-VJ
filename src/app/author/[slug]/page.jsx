00c6c1a3e02da9d85823dfaaae55405b4b4d1687
Ashish <dev@concealedwines.com>
2024-09-27 11:12:14 +0545

    ashish author page repush

---
+++ src/app/author/[slug]/page.jsx
@@ -0,0 +1,37 @@
+
+import SenasteNytt from "../../../../components/SenasteNytt";
+import WineTourism from "../../../../components/WineTourism";
+import Card from "../../Components/Card";
+import Footer from "../../Components/Footer";
+import Info from "../../Components/Info";
+import Navbar from "../../Components/Navbar";
+import Subscription from "../../Components/Subscription";
+import AuthorHero from "../Components/AuthorHero"; 
+import CircleImage from "../Components/CircleImage";
+
+export default function Author() {
+  return (
+    <div>
+      <Navbar />
+      <AuthorHero 
+        title='Author'
+      />
+      <CircleImage />
+      <Card
+        title="FROM AUTHOR"
+        subtitle="Read more articles from Jeanetter Gardner"
+      />
+      <Subscription/>
+      <div className="flex flex-col md:flex-row container mx-auto">
+        <div className="flex items-center md:w-1/2 p-10">
+          <WineTourism/>
+        </div>
+            
+        <div className="flex items-center md:w-1/2 p-10">
+          <SenasteNytt/>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}

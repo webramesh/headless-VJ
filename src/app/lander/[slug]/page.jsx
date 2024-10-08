18b2a10fcc23984398e00d3e58ac6e67f0ac61a7
Adro1t <ramankhadgi0@gmail.com>
2024-10-08 15:32:33 +0545

    producenter-page

--- src/app/lander/[slug]/page.jsx
+++ src/app/lander/[slug]/page.jsx
@@ -1,4 +1,3 @@
-import Map from "../../Components/Map";
 import LanderContent from "../components/LanderContent";
 
 function page({ params }) {
---
+++ src/app/vin-producenter/page.jsx
@@ -0,0 +1,30 @@
+import Map from "../Components/Map";
+import Pagination from "../Components/Pagination";
+import ProducenterCard from "../Components/producenterCard/ProducenterCard";
+
+function page() {
+  return (
+    <>
+      <div className="font-outfit text-xs lg:text-sm flex gap-1 my-2">
+        Hem » Producenter
+      </div>
+      <h1 className="text-2xl lg:text-3xl font-outfit mb-4 font-bold uppercase ">
+        Producenter
+      </h1>
+      <div className="h-96">
+        <Map />
+      </div>
+      <div className="mt-10 grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {Array.from({ length: 15 }, (_, i) => (
+          <div key={i}>
+            <ProducenterCard />
+          </div>
+        ))}
+        {/* pagination */}
+      </div>
+      <Pagination />
+    </>
+  );
+}
+
+export default page;

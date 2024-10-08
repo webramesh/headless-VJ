30124340305b5c2a926b8c36a6d29a9a3c94cb41
Adro1t <ramankhadgi0@gmail.com>
2024-10-08 11:59:17 +0545

    lander slug page completed

--- src/app/lander/[slug]/layout.jsx
+++ src/app/lander/[slug]/layout.jsx
@@ -1,7 +1,5 @@
 import Footer from "../../Components/Footer";
-import Map from "../../Components/Map";
 import Navbar from "../../Components/Navbar";
-import Sidebar from "../../Components/Sidebar";
 import SubscriptionForm from "../../Components/subscription/SubscriptionForm";
 import CatAccordion from "../../[category]/Components/CatAccordion";
 
@@ -9,14 +7,16 @@ function layout({ children }) {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto flex gap-5 mt-10">
-        <div className="space-y-4 md:w-2/3 font-outfit ">
-          {children}
-          <SubscriptionForm />
-          <CatAccordion />
-        </div>
-        <div className="w-[30%] hidden md:block">
-          <Sidebar />
+      <div className="container mx-auto flex gap-5 mt-4 lg:mt-10 ">
+        <div className="space-y-4 font-outfit ">
+          <div className="container mx-auto px-4 lg:px-0">{children}</div>
+
+          <div className="mx-4 lg:mx-52 lg:-mt-16 z-10">
+            <div className="flex flex-col gap-10 bg-white w-full lg:w-auto">
+              <SubscriptionForm />
+              <CatAccordion />
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
--- src/app/lander/[slug]/page.jsx
+++ src/app/lander/[slug]/page.jsx
@@ -5,9 +5,6 @@ function page({ params }) {
   return (
     <>
       <LanderContent params={params} />
-      <div className="h-96">
-        <Map />
-      </div>
     </>
   );
 }

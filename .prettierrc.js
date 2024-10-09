c739f2069ede0b02f87d12214e746af3fc8c7c07
webramesh <webdramesh@gmail.com>
2024-10-09 09:28:21 +0545

    prettier and eslint config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'es5',
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 100,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    endOfLine: 'lf'
+  };

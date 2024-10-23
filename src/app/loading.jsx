d6fbae91434aab96f5f16d30e8b2f7801c0e7045
ramesh-kunwar <rameshkunwar1110@gmail.com>
2024-10-23 11:23:46 +0545

    main page converted to server component

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SkeletonLoader from './Components/SkeletonLoading/SkeletonLoader';
+
+const Loading = () => {
+    return (
+        <>
+            <SkeletonLoader />
+        </>
+    );
+};
+
+export default Loading;
--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,35 @@
-'use client';
 import Head from 'next/head';
-import Navbar from '../app/Components/Navbar.jsx';
-import React from 'react';
-import Hero from './Components/Hero.jsx';
-import Card from './Components/Card.jsx';
-import Subscription from './Components/Subscription.jsx';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Card from './Components/Card';
+import Subscription from './Components/Subscription';
+import Info from './Components/Info';
+import Footer from './Components/Footer';
 import Wine from './Components/Wine.jsx';
-import SkeletonLoader from './Components/SkeletonLoading/SkeletonLoader.jsx';
-import { useState, useEffect } from 'react';
-import Info from './Components/Info.jsx';
-import Footer from './Components/Footer.jsx';
+import { Suspense } from 'react';
+import Loading from './loading';
 
-export default function Home() {
-  const [loading, setLoading] = useState(true);
+const HomePage = () => {
+    return (
+        <div>
+            <Suspense fallback={<Loading />}>
+                <Head>
+                    <title>Home Page</title>
+                    <meta name="description" content="This is the home page of my Next.js app" />
+                </Head>
+                <Navbar />
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    });
-    return () => clearTimeout(timer);
-  }, []);
+                <Hero />
+                <Card title="TRENDIGT" subtitle="Artiklar värda att läsa från våra redaktörer" />
+                <Subscription />
+                <Wine />
+                <Card title="NYHETER" subtitle="Den mest populära artikeln i dryckesvärlden" />
+                <Info />
+            </Suspense>
 
-  return (
-    <>
-      <Head>
-        <title>Home Page</title>
-        <meta name="description" content="This is the home page of my Next.js app" />
-      </Head>
-      {loading ? (
-        <SkeletonLoader />
-      ) : (
-        <>
-          <Navbar />
-          <Hero />
-          <Card title="TRENDIGT" subtitle="Artiklar värda att läsa från våra redaktörer" />
-          <Subscription />
-          <Wine />
-          <Card title="NYHETER" subtitle="Den mest populära artikeln i dryckesvärlden" />
-          <Info />
-          <Footer />
+            <Footer />
+        </div>
+    );
+};
 
-          {/* post */}
-          {/* trending */}
-        </>
-      )}
-    </>
-  );
-}
+export default HomePage;

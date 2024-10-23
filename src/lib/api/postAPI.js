28d523921c27de3d4ae6538db870240b6b5aa04e
ramesh-kunwar <rameshkunwar1110@gmail.com>
2024-10-23 11:43:14 +0545

    graphql api added for post

--- src/lib/api/postAPI.js
+++ src/lib/api/postAPI.js
@@ -1,1 +1,44 @@
 // post api
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: process.env.SITE_URL_ENDPOINT,
+    cache: new InMemoryCache(),
+});
+
+export async function getHomePagePosts() {
+    try {
+        const { data } = await client.query({
+            query: gql`
+                query AllPosts {
+                    posts(first: 5) {
+                        nodes {
+                            author {
+                                node {
+                                    name
+                                }
+                            }
+                            date
+                            title
+                            excerpt
+                            modified
+                            id
+                            slug
+                            visitCount
+                            featuredImage {
+                                node {
+                                    mediaItemUrl
+                                }
+                            }
+                        }
+                    }
+                }
+            `,
+        });
+
+        return data.posts.nodes;
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        return [];
+    }
+}

4242f5bed5b533f2e14c5429ff4b2c880c5d9feb
Adro1t <ramankhadgi0@gmail.com>
2024-10-30 16:10:07 +0545

    regioner-slug-page api implemented

--- src/app/regioner/[country]/[region]/page.jsx
+++ src/app/regioner/[country]/[region]/page.jsx
@@ -1,13 +1,14 @@
 import Map from '@/src/app/Components/Map';
 import Image from 'next/image';
-import Link from 'next/link';
-import lugana from '@/public/Lugana.png';
 import BreadCrumb from '@/src/app/Components/breadcrumb/BreadCrumb';
+import { getRegionByURL } from '@/src/lib/api/regionerAPI';
 
-function page({ params }) {
+async function page({ params }) {
   const { country, region } = params;
+  const selectedRegion = await getRegionByURL(`/regioner/${region}`);
+  const { featuredImage, content } = selectedRegion;
   return (
-    <div>
+    <>
       <h1 className="text-2xl lg:text-3xl  font-semibold uppercase">{region}</h1>
       <BreadCrumb
         title1="Regioner"
@@ -20,25 +21,19 @@ function page({ params }) {
       <div className="h-80">
         <Map />
       </div>
-      <Image src={lugana} alt="region picture" className="w-full my-4 lg:my-10 object-contain" fill={false} />
-      <p className="text-sm lg:text-base mb-1 lg:mb-2">
-        Här odlas främst de röda druvorna Sangiovese och Montepulciano och de vita Trebbiano och Malvasia. Röda viner
-        från Abruzzo är fylliga och smakrika och har fin färg. Vita viner från Abruzzo är torra och syrliga. DOC
-        Montepulciano de Abruzzo och DOC Trebbiano d’Abruzzo är de mest kända områdena. Abruzzo har Italiens högsta
-        tillåtna skördeuttag, vilket inte är positivt ur kvalitetssynpunkt.
-      </p>
-      <p className="text-sm lg:text-base mb-1 lg:mb-2">
-        Är du intresserade av att besöka regionen Abruzzo?&nbsp;
-        <Link
-          href="https://www.winetourism.com/wine-tasting-tours-in-abruzzo/"
-          className="text-red-500"
-          target="_blank"
-        >
-          Boka enkelt vinprovning och vingårdsbesök i Abruzzo
-        </Link>
-        &nbsp; via WineTourism.com, en sida för vinturism på nätet.
-      </p>
-    </div>
+      {featuredImage?.node?.sourceUrl && (
+        <Image
+          src={featuredImage?.node?.sourceUrl}
+          alt={region}
+          className="w-full my-4 lg:my-10 object-contain "
+          width={250}
+          height={250}
+        />
+      )}
+      <div className="text-sm lg:text-base mb-1 lg:mb-2">
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </>
   );
 }
 

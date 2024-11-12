3de2e2c7adad6ce7e3f5c51dc8dc6ae31b88a2bc
Adro1t <ramankhadgi0@gmail.com>
2024-11-12 12:38:51 +0545

    pagination in category page

--- src/app/[category]/CategoryPage.jsx
+++ src/app/[category]/CategoryPage.jsx
@@ -1,72 +1,54 @@
 'use client';
-
-import React from 'react';
-import Link from 'next/link';
-import { ChevronLast, FileInput, FileOutput, ChevronFirst, ChevronsRight, ChevronsLeft } from 'lucide-react';
-import PostTypeContent from '../../app/Components/PostTypeContent';
-import Sidebar from '../../app/Components/Sidebar';
-import CategoryCard from '../../app/Components/CategoryCard';
+import { usePagination } from '@/src/context/PageContext';
 import AccordionNew from '../../app/Components/AccordionNew';
-import CatAccordion from '../[category]/Components/CatAccordion';
-import SubscriptionForm from '../../app/Components/subscription/SubscriptionForm';
+import { useEffect, useState } from 'react';
+import { getPostsByCategory } from '@/src/lib/api/postAPI';
+import Pagination from '../Components/pagination/Pagination';
+import CategoryCard from '../Components/CategoryCard';
 
-const PaginationButton = ({ href, disabled, children }) => (
-  <Link
-    href={href}
-    className={`flex items-center justify-center w-10 h-10 rounded-full ${
-      disabled
-        ? 'bg-gray-200 text-gray-400 cursor-not-allowed'
-        : 'bg-red-500 text-white hover:bg-red-600 transition-colors duration-200 opacity-80'
-    }`}
-    aria-disabled={disabled}
-  >
-    {children}
-  </Link>
-);
+const POSTS_PER_PAGE = 15;
 
-export default function CategoryPage({ posts, categoryName, categoryDescription, page, totalPages, category }) {
+export default function CategoryPage({ category, totalPosts }) {
+  const { state, dispatch } = usePagination();
+  const { after, before, first, last } = state;
+  const [posts, setPosts] = useState([]);
+  const [pageInfo, setPageInfo] = useState({});
+  const [isReset, setIsReset] = useState(false);
+
+  useEffect(() => {
+    dispatch({ type: 'RESET', payload: POSTS_PER_PAGE });
+    setIsReset(true);
+  }, [dispatch]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      dispatch({ type: 'CHANGE_LOADING', payload: true });
+
+      const { posts, pageInfo } = await getPostsByCategory(category, first, last, after, before);
+      if (posts && pageInfo) {
+        setPosts(posts);
+        setPageInfo(pageInfo);
+        dispatch({ type: 'CHANGE_LOADING', payload: false });
+      } else {
+        console.warn('No products or page info returned from getAllOrdlista');
+      }
+    };
+
+    if (isReset) fetchPosts();
+  }, [after, before, first, isReset, last, dispatch, category]);
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="flex flex-col lg:flex-row lg:gap-10">
-        {/* Main Content Section (3/4) */}
-        <div className="w-full lg:w-3/4 flex flex-col gap-6">
-          <PostTypeContent
-            title={categoryName || 'Online'}
-            description={categoryDescription}
-            fallbackText="Sedan 2007 är det lagligt att importera vin privat via nätet i Sverige. Om du funderar på att ta vara på det utökade utbudet och de ofta billigare priserna guidar vi dig genom processen. Vi förklara steg för steg hur du går till väga och listar annan viktig information."
-          />
-
-          <div className="space-y-4">
-            <CategoryCard posts={posts} />
-            {/* Pagination */}
-            <nav className="flex justify-center items-center space-x-2 py-8" aria-label="Pagination">
-              <PaginationButton href={`/${category}?page=1`} disabled={page <= 1}>
-                <ChevronsLeft className="w-6 h-6" />
-              </PaginationButton>
-              <PaginationButton href={`/${category}?page=${page - 1}`} disabled={page <= 1}>
-                <ChevronFirst className="w-6 h-6" />
-              </PaginationButton>
-              <span className="px-4 py-2 rounded-md bg-gray-100 text-gray-700">
-                Page {page} of {totalPages}
-              </span>
-              <PaginationButton href={`/${category}?page=${page + 1}`} disabled={page >= totalPages}>
-                <ChevronLast className="w-6 h-6" />
-              </PaginationButton>
-              <PaginationButton href={`/${category}?page=${totalPages}`} disabled={page >= totalPages}>
-                <ChevronsRight className="w-6 h-6" />
-              </PaginationButton>
-            </nav>
-            <AccordionNew />
-            <SubscriptionForm />
-            <CatAccordion />
+    <div className="space-y-4">
+      <div className=" mt-10 p-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <CategoryCard post={post} />
           </div>
-        </div>
-
-        {/* Sidebar Section (1/4) */}
-        <div className="w-1/4 hidden lg:block sticky top-0 h-full mt-12">
-          <Sidebar />
-        </div>
+        ))}
       </div>
+      {/* Pagination */}
+      <Pagination pageInfo={pageInfo} pageLimit={POSTS_PER_PAGE} total={totalPosts} />
+
+      <AccordionNew />
     </div>
   );
 }
--- src/app/[category]/[slug]/page.jsx
+++ src/app/[category]/[slug]/page.jsx
@@ -3,23 +3,23 @@ import PostDetailsContent from '../Components/PostDetailsContent';
 import PostDetailsHero from '../Components/PostDetailsHero';
 
 export default async function PostDetails({ params }) {
-  const { category, slug } = params;
+  const { slug } = params;
   const post = await getPostBySlug(slug);
   if (!post) {
     return <div className="container mx-auto px-4 py-8">Post not found</div>;
   }
 
   return (
-    <div className="">
+    <>
       <PostDetailsHero
         title={post?.title}
         featuredImage={post?.featuredImage?.node?.sourceUrl}
         authorImage={post?.author?.node?.customAvatar}
         authorName={post?.author?.node?.name}
         date={post?.date}
-        categoryName={post?.categories?.nodes[0]?.name}
+        category={post?.categories?.nodes[0]}
       />
       <PostDetailsContent content={post?.content} />
-    </div>
+    </>
   );
 }

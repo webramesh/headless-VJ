908686ac6c35bc492a9e965cbbc6553d6ad21123
ramesh-kunwar <rameshkunwar1110@gmail.com>
2024-11-21 12:44:50 +0545

    product by slug - made dynamic, and added download pdf feature

--- src/app/produkter/[slug]/page.jsx
+++ src/app/produkter/[slug]/page.jsx
@@ -2,6 +2,8 @@ import ProductSection from '../Components/ProductSection';
 import InformationCards from '../Components/InformationCards';
 import Price from '../Components/Price';
 import { getProductBySlug } from '@/src/lib/api/productsAPI';
+import SubscriptionForm from '../../Components/subscription/SubscriptionForm';
+import SubscriptionBox from '../../Components/subscription/SubscriptionBox';
 
 export default async function page({ params }) {
   const product = await getProductBySlug(params.slug);
@@ -11,6 +13,16 @@ export default async function page({ params }) {
       <ProductSection product={product} />
       <InformationCards fieldsProduct={product.fieldsProduct} />
       <Price />
+
+      <div className="px-8 container mx-auto block md:grid grid-cols-6 items-center justify-between gap-8 ">
+        <div className="col-span-4">
+          <SubscriptionForm />
+        </div>
+        <div className="w-full grid col-span-2">
+          {/* <SubscriptionBox ordlista={ordlista} /> */}
+          <SubscriptionBox />
+        </div>
+      </div>
     </>
   );
 }

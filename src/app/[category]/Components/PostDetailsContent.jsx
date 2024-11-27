e85a182e8dc6589543fc93c997081463ec150b71
Prakriti Adhikari <prakritiadhikari2121@gmail.com>
2024-11-27 17:10:26 +0545

    Post Details Content layout changed[C

--- src/app/[category]/Components/PostDetailsContent.jsx
+++ src/app/[category]/Components/PostDetailsContent.jsx
@@ -6,7 +6,9 @@ const PostDetailsContent = ({ content }) => {
       <div className="flex flex-col lg:flex-row lg:gap-16 mx-4 lg:mx-52 lg:-mt-16 z-10">
         <div className="flex flex-col gap-2 bg-white w-full lg:w-auto">
           <div className="shadow-2xl p-4 lg:p-16">
+            <div className='content'>
             <div className="prose prose-lg max-w-none" dangerouslySetInnerHTML={{ __html: content }} />
+            </div>
           </div>
         </div>
       </div>

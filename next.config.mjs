dafbf9ed96c4ac973e9955f64b58f3fff5cb54c2
Adro1t <ramankhadgi0@gmail.com>
2024-12-23 16:28:08 +0545

    redirection from old to new links

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,61 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      //lander
+      {
+        source: '/vin-atlas',
+        destination: '/lander',
+        permanent: true,
+      },
+      //producenter
+      {
+        source: '/vin-producenter',
+        destination: '/producenter',
+        permanent: true,
+      },
+      //regioner
+      {
+        source: '/vin-regioner',
+        destination: '/regioner',
+        permanent: true,
+      },
+      //ordlista
+      {
+        source: '/ordlista-kategori',
+        destination: '/ordlista',
+        permanent: true,
+      },
+      {
+        source: '/ordlista-kategori/:slug',
+        destination: '/ordlista/:slug',
+        permanent: true,
+      },
+      //produktTyper
+      {
+        source: '/produkt-typer/vin/:slug',
+        destination: '/produkt-typer/:slug',
+        permanent: true,
+      },
+      //produktLand
+      {
+        source: '/produkt-land/:country/:region',
+        destination: '/produkt-land/:region',
+        permanent: true,
+      },
+      {
+        source: '/produkt-land/:country/:region/:subregion',
+        destination: '/produkt-land/:subregion',
+        permanent: true,
+      },
+      {
+        source: '/produkt-land/:country/:region/:subregion/:wine',
+        destination: '/produkt-land/:wine',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
